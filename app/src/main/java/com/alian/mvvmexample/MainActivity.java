de9935b55234f415cd6c7816a2962df07b37fb37
ilyasyasin <ilyasyasin072@gmail.com>
2020-12-02 08:56:37 +0700

    Add Note Activity

--- app/src/main/java/com/alian/mvvmexample/MainActivity.java
+++ app/src/main/java/com/alian/mvvmexample/MainActivity.java
@@ -1,46 +1,72 @@
 package com.alian.mvvmexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.alian.mvvmexample.adapter.NoteAdapter;
 import com.alian.mvvmexample.model.Note;
 import com.alian.mvvmexample.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NoteAdapter adapter;
+    private FloatingActionButton fbAdd;
+    private static final int ADD_NOTE_REQUEST = 1;
     private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ui();
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, notes -> {
+            // update Recycler View
+            adapter.setNotes(notes);
+            Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+        });
 
+        fbAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddNoteActivity.class);
+            startActivityForResult(intent, ADD_NOTE_REQUEST);
+        });
+    }
+
+    private void ui() {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        NoteAdapter adapter = new NoteAdapter();
+        adapter = new NoteAdapter();
+        fbAdd = findViewById(R.id.fabAdd);
         recyclerView.setAdapter(adapter);
+    }
 
-        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
-        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
-            @Override
-            public void onChanged(List<Note> notes) {
-                // update Recycler View
-                adapter.setNotes(notes );
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, description, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
